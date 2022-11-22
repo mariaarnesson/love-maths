80803be76260d2e0a64cfa7e9c42d0c19d0ec24c
mariaarnesson <maria_arnesson@outlook.com>
2022-11-22 11:01:01 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,8 @@ function runGame(gameType){
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
 
     } else {
         alert(`Unknown game type: ${gameType}`);
@@ -66,6 +68,8 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
 
     }else {
             alert(`Unimplemented operator ${operator}`);
@@ -100,7 +104,11 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion() {
 }
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+    
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 
 }
 function displayDivisionQuestion() {
